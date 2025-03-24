763f1a111b748a36239a2e3c2ca1b135465588c6
JSaaski <c3sajo01@students.oamk.fi>
2025-03-24 15:56:36 +0200

    aloitusvalikon pohja

--- App.js
+++ App.js
@@ -1,20 +1,57 @@
-import { StatusBar } from 'expo-status-bar';
-import { StyleSheet, Text, View } from 'react-native';
+import React from 'react';
+import { View, Text, TouchableOpacity, StyleSheet } from 'react-native';
+import Ionicons from '@expo/vector-icons/Ionicons';
 
-export default function App() {
+export default function App(){
   return (
     <View style={styles.container}>
-      <Text>Open up App.js to start working on your app!</Text>
-      <StatusBar style="auto" />
+      <Text style={styles.title}>SPEDEN NOPEUSTESTI</Text>
+      <View style={styles.buttonContainer}>
+        <TouchableOpacity style={[styles.button, { backgroundColor: 'green' }]}> 
+          <Ionicons name="play" size={32} color="black" />
+        </TouchableOpacity>
+        <TouchableOpacity style={[styles.button, { backgroundColor: 'red' }]}> 
+          <Ionicons name="help" size={32} color="black" />
+        </TouchableOpacity>
+        <TouchableOpacity style={[styles.button, { backgroundColor: 'blue' }]}> 
+          <Ionicons name="trophy" size={32} color="black" />
+        </TouchableOpacity>
+        <TouchableOpacity style={[styles.button, { backgroundColor: 'yellow' }]}> 
+          <Ionicons name="settings" size={32} color="black" />
+        </TouchableOpacity>
+      </View>
     </View>
   );
-}
+};
 
 const styles = StyleSheet.create({
   container: {
     flex: 1,
-    backgroundColor: '#fff',
-    alignItems: 'center',
+    backgroundColor: '#D9D9D9',
     justifyContent: 'center',
+    alignItems: 'center',
+  },
+  title: {
+    position: "absolute",
+    top: 20,
+    fontSize: 24,
+    fontWeight: 'bold',
+    color: '#BD1C1C',
+  },
+  buttonContainer: {
+    position: "absolute",
+    bottom: 50,
+    flexDirection: 'row',
+    justifyContent: 'space-around',
+    width: '80%',
+  },
+  button: {
+    width: 80,
+    height: 80,
+    borderRadius: 50,
+    justifyContent: 'center',
+    alignItems: 'center',
+    marginHorizontal: 10,
   },
 });
+
