ded0bdd6b467c9e487bef71f311f81ab79c89eee
Mika Lohi <152972334+Moti555@users.noreply.github.com>
2025-04-02 17:29:53 +0300

    muokattu popuppia

--- screens/HelpPopup.js
+++ screens/HelpPopup.js
@@ -1,26 +1,40 @@
-import { View, Text, Modal, StyleSheet, ScrollView } from 'react-native';
-import React from 'react';
+import { View, Text, Modal, StyleSheet, ScrollView, TouchableOpacity } from 'react-native';
+import { useNavigation } from '@react-navigation/native';
+import React, { useEffect } from 'react';
 import Ionicons from '@expo/vector-icons/Ionicons'; 
 import HapticButton from "../components/hapticButton";
 
-const HelpPopup = ({ visible, onClose }) => { 
+const HelpPopup = ({ visible, setVisible, advice }) => { 
+  const navigation = useNavigation();
+
+  useEffect(() => {
+    const beforeRemoveListener = (e) => {
+      e.preventDefault();
+      setVisible(false);
+    };
+    navigation.addListener('beforeRemove', beforeRemoveListener);
+
+    return () => {
+      navigation.removeListener('beforeRemove', beforeRemoveListener);
+    };
+  }, [navigation, setVisible]);
+
   return ( 
     <Modal
       animationType="fade"
       transparent={true}
       visible={visible}
-      onRequestClose={onClose}
+      onRequestClose={() => setVisible(false)}
     >
       <View style={styles.overlay}>
         <View style={styles.container}> 
           <Text style={styles.title}>SPEDEN NOPEUSTESTIN OHJEET</Text> 
           <ScrollView style={styles.scrollView}> 
-            <Text style={styles.text}>1. Paina nappia, jossa valo on palanut mahdollisimman nopeaa</Text>
-            <Text style={styles.text}>2. etc.</Text>
+            <Text style={styles.text}>{advice}</Text>
           </ScrollView>
           <View style={styles.buttonContainer}>
-            <HapticButton style={[styles.button, { backgroundColor: '#CEDB1B' }]} onPress={onClose}>
-              <Ionicons name="arrow-back" size={42} color="black" />
+            <HapticButton style={styles.backButton} onPress={() => setVisible(false)}>
+              <Text style={styles.backButtonText}>Back</Text>
             </HapticButton>
           </View>
         </View>
@@ -32,14 +46,14 @@ const HelpPopup = ({ visible, onClose }) => {
 const styles = StyleSheet.create({
   overlay: {
     flex: 1,
-    backgroundColor: 'rgba(255, 255, 255, 0)',
+    backgroundColor: 'rgba(0, 0, 0, 0.5)',
     justifyContent: 'center',
     alignItems: 'center',
   },
   container: {
-    width: '50%',
+    width: '80%',
     backgroundColor: 'green',
-    borderRadius: 5,
+    borderRadius: 10,
     padding: 20,
     alignItems: 'center',
   },
@@ -57,23 +71,33 @@ const styles = StyleSheet.create({
     height: 60,
     borderRadius: 30,
     borderWidth: 2.5,
-    borderColor: "green",
+    borderColor: 'green',
     justifyContent: 'center',
     alignItems: 'center',
   },
   scrollView: {
     width: '100%',
     maxHeight: 150,
-    backgroundColor: "green",
+    backgroundColor: 'green',
     borderRadius: 10,
     borderWidth: 2,
-    borderColor: "green",
+    borderColor: 'green',
     padding: 5,
   },
   text: {
-    color: "white",
+    color: 'white',
     fontSize: 15,
-  }
+  },
+  backButton: {
+    backgroundColor: "#CEDB1B",
+    padding: 10,
+    borderRadius: 5,
+  },
+  backButtonText: {
+    fontSize: 16,
+    fontWeight: "bold",
+    color: "#000",
+  },
 });
 
 export default HelpPopup;
