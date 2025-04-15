773c0b6cf340c92dc9bb6f87a5c8c3450a3845e8
JSaaski <c3sajo01@students.oamk.fi>
2025-04-15 23:31:53 +0300

    ennatyssivun uusi versio

--- firebase/saveHighscore.js
+++ firebase/saveHighscore.js
@@ -4,9 +4,9 @@ import { db } from './Config';
 export const saveHighscore = async (score, difficulty) => {
   try {
     const highscoreData = {
-      score,
-      timestamp: serverTimestamp(),
-      difficulty
+      score, // pelaajan score
+      timestamp: serverTimestamp(), // serverin kellon aika
+      difficulty // pelin vaikeustaso
     };
 
     await addDoc(collection(db, 'highscores'), highscoreData);
